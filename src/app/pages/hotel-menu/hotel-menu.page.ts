24dd7c5f9be062fb4a9f57c25ad60de800294e04
imanzal <ansalkhan.k.a@lxisoft.com>
2019-06-14 11:36:55 +0530

    config fix

--- src/app/pages/hotel-menu/hotel-menu.page.ts
+++ src/app/pages/hotel-menu/hotel-menu.page.ts
@@ -1,5 +1,4 @@
 import { Subscription } from 'rxjs';
-import { OrderLine } from './../../api/models/order-line';
 import { CommandResourceService } from 'src/app/api/services';
 import { RatingReview } from './../../api/models/rating-review';
 import { UserRatingDTO } from './../../api/models/user-rating-dto';
@@ -8,24 +7,23 @@ import { CartService } from './../../services/cart.service';
 import { StockCurrent } from './../../api/models/stock-current';
 import { Store } from './../../api/models/store';
 import { HotelMenuPopoverComponent } from './../../components/hotel-menu-popover/hotel-menu-popover.component';
-import { HotelMenuPageModule } from './hotel-menu.module';
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
-import { PopoverController, IonSlide, IonSlides, ToastController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  PopoverController,
+  IonSlides,
+  ToastController
+} from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { QueryResourceService } from 'src/app/api/services/query-resource.service';
 import { UserRating } from 'src/app/api/models/user-rating';
-import { ReviewDTO, TicketLineDTO, Product } from 'src/app/api/models';
+import { ReviewDTO, Product } from 'src/app/api/models';
 import {
   GoogleMaps,
   GoogleMap,
-  GoogleMapsEvent,
   GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
   Marker,
   Environment,
-  MyLocation,
   GoogleMapsAnimation,
   LatLng
 } from '@ionic-native/google-maps';
@@ -34,18 +32,19 @@ import { Loading } from 'src/app/components/loading';
 @Component({
   selector: 'app-hotel-menu',
   templateUrl: './hotel-menu.page.html',
-  styleUrls: ['./hotel-menu.page.scss'],
+  styleUrls: ['./hotel-menu.page.scss']
 })
 export class HotelMenuPage implements OnInit {
-
-  constructor(private popoverController: PopoverController,
+  constructor(
+    private popoverController: PopoverController,
     private route: ActivatedRoute,
     private cartService: CartService,
     private toastController: ToastController,
     private oauthService: OAuthService,
     private commandResourceService: CommandResourceService,
     private queryResourceService: QueryResourceService,
-    private loadingCreator: Loading) { }
+    private loadingCreator: Loading
+  ) {}
   storeId;
   map: GoogleMap;
   store: Store;
@@ -63,72 +62,98 @@ export class HotelMenuPage implements OnInit {
   ratings: UserRating[];
   categories: Category[];
   rate: UserRatingDTO = { rating: 1 };
-  review: ReviewDTO = { userName: '', review: '', reviewedDate: '', storeId: 0 };
+  review: ReviewDTO = {
+    userName: '',
+    review: '',
+    reviewedDate: '',
+    storeId: 0
+  };
   stockCurrents: StockCurrent[];
   products: Product[] = [];
-  selectedCategory: string = 'All';
+  selectedCategory = 'All';
   now: number;
   loading: HTMLIonLoadingElement;
   @ViewChild('slides') slides: IonSlides;
 
   ngOnInit() {
-    this.loadingCreator.createLoader()
-    .then(data => {
+    this.loadingCreator.createLoader().then(data => {
       this.loading = data;
       this.loading.present();
       this.storeId = this.route.snapshot.paramMap.get('id');
       this.cartService.storeId = this.storeId;
-      this.queryResourceService.findStoreByRegisterNumberUsingGET(this.storeId).subscribe(result => {
-        this.store = result;
-      }, err => {
-        console.log('Error fetching store data', err);
-      });
-      this.queryResourceService.findStockCurrentByStoreIdUsingGET(this.storeId).subscribe(result => {
-        this.stockCurrents = result;
-        result.forEach(() => {
-          this.cardExpand.push(0);
-          this.loading.dismiss();
+      this.queryResourceService
+        .findStoreByRegisterNumberUsingGET(this.storeId)
+        .subscribe(
+          result => {
+            this.store = result;
+          },
+          err => {
+            console.log('Error fetching store data', err);
+          }
+        );
+      this.queryResourceService
+        .findStockCurrentByStoreIdUsingGET(this.storeId)
+        .subscribe(
+          result => {
+            this.stockCurrents = result;
+            result.forEach(() => {
+              this.cardExpand.push(0);
+              this.loading.dismiss();
+            });
+          },
+          err => {
+            console.log('Error fetching product data', err);
+          }
+        );
+      this.queryResourceService
+        .findRatingReviewByStoreidAndCustomerNameUsingGET({
+          storeId: this.storeId
+        })
+        .subscribe(
+          result => {
+            this.rateReview = result.content;
+          },
+          err => {
+            console.log('Error fetching review data', err);
+          }
+        );
+      this.queryResourceService
+        .findCategoryByStoreIdUsingGET({ userId: this.storeId })
+        .subscribe(success => {
+          this.categories = success.content;
         });
-      }, err => {
-        console.log('Error fetching product data', err);
-      });
-      this.queryResourceService.findRatingReviewByStoreidAndCustomerNameUsingGET({ storeId: this.storeId }).subscribe(result => {
-        this.rateReview = result.content;
-      }, err => {
-        console.log('Error fetching review data', err);
-      });
-      this.queryResourceService.findCategoryByStoreIdUsingGET({ userId: this.storeId }).subscribe(success => {
-        this.categories = success.content;
-      });
       this.oauthService.loadUserProfile().then(user => {
         this.usr = user;
       });
-      this.subscriptionCart = this.cartService.observableTickets.subscribe(orderLines => this.cartSize = orderLines.length);
-      this.subscriptionPrice = this.cartService.observablePrice.subscribe(price => this.totalPrice = price);
-      this.timeTracker();  
-    })
+      this.subscriptionCart = this.cartService.observableTickets.subscribe(
+        orderLines => (this.cartSize = orderLines.length)
+      );
+      this.subscriptionPrice = this.cartService.observablePrice.subscribe(
+        price => (this.totalPrice = price)
+      );
+      this.timeTracker();
+    });
   }
 
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
       component: HotelMenuPopoverComponent,
       componentProps: {
-        categories: this.categories, storeId: this.storeId
-        , selectedCategory: this.selectedCategory
+        categories: this.categories,
+        storeId: this.storeId,
+        selectedCategory: this.selectedCategory
       },
       event: ev,
       translucent: true
     });
-    popover.onDidDismiss()
-      .then((data: any) => {
-        if (data.data.result !== undefined) {
-          this.stockCurrents = data.data.result;
-          this.selectedCategory = data.data.selectedCategory;
-        } else {
-          this.presentToast('Error while Getting data');
-        }
-
-      })
+    popover.onDidDismiss().then((data: any) => {
+      if (data.data.result !== undefined) {
+        this.stockCurrents = data.data.result;
+        this.selectedCategory = data.data.selectedCategory;
+      } else {
+        this.presentToast('Error while Getting data');
+      }
+    });
     return await popover.present();
   }
 
@@ -167,11 +192,16 @@ export class HotelMenuPage implements OnInit {
       const raterev: RatingReview = { review: this.review, rating: this.rate };
       raterev.rating.userName = this.usr.preferred_username;
       raterev.review.userName = this.usr.preferred_username;
-      this.commandResourceService.createRatingAndReviewUsingPOST({ ratingReview: raterev }).subscribe(result => {
-        this.rateReview = result.content;
-      }, err => {
-        this.presentToast('Error while posting review. Try again later');
-      });
+      this.commandResourceService
+        .createRatingAndReviewUsingPOST({ ratingReview: raterev })
+        .subscribe(
+          result => {
+            this.rateReview = result.content;
+          },
+          err => {
+            this.presentToast('Error while posting review. Try again later');
+          }
+        );
     } else {
       this.presentToast('Review field can\'t be empty.');
     }
@@ -231,26 +261,38 @@ export class HotelMenuPage implements OnInit {
   searchProducts(event) {
     if (event.detail.value !== '') {
       const query: string = event.detail.value;
-      this.queryResourceService.findAllStockCurrentByProductNameStoreIdUsingGET({ name: query.toLowerCase(), storeId: this.storeId })
-        .subscribe(res => {
-          this.stockCurrents = res;
-        }, err => {
-          this.presentToast('No results found');
-        });
+      this.queryResourceService
+        .findAllStockCurrentByProductNameStoreIdUsingGET({
+          name: query.toLowerCase(),
+          storeId: this.storeId
+        })
+        .subscribe(
+          res => {
+            this.stockCurrents = res;
+          },
+          err => {
+            this.presentToast('No results found');
+          }
+        );
     } else {
-      this.queryResourceService.findStockCurrentByStoreIdUsingGET(this.storeId).subscribe(result => {
-        this.stockCurrents = result;
-      }, err => {
-        console.log('Error fetching product data', err);
-      });
+      this.queryResourceService
+        .findStockCurrentByStoreIdUsingGET(this.storeId)
+        .subscribe(
+          result => {
+            this.stockCurrents = result;
+          },
+          err => {
+            console.log('Error fetching product data', err);
+          }
+        );
     }
   }
 
-  // I dont Know/not sure whether this 
+  // I dont Know/not sure whether this
   // function will cause any Performance issues
   timeTracker() {
     setInterval(() => {
-      let date = new Date()
+      const date = new Date();
       this.now = this.getTimeFixed(date.getHours() + '.' + date.getMinutes());
     }, 1000);
   }
@@ -259,8 +301,7 @@ export class HotelMenuPage implements OnInit {
     return parseFloat(str.replace(':', '.'));
   }
 
-  timeDifference(ot , ct) {
-
+  timeDifference(ot, ct) {
     // Calculate difference between time
     return 4.0;
   }
